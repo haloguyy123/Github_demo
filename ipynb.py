080a19d1fcf82e6b51d3d60bffb802fa24968324
haloguyy123 <67880257+haloguyy123@users.noreply.github.com>
2020-07-07 10:52:36 -0400

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print('hello world!')
+Name=input('What is your name?')
+print('Its nice to meet you',Name)
+Food=input('What is your favorite food?')
+print('Oh! I like', Food, 'too!')
+Color=input('What is your favorite color?')
+print (Color,'is a great color but I think I like red better.')
+Mood=input('How are you feeling today?')
+
+
+
+
+
+
+
+
+
