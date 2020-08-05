6846531f8450203a60a3740dc28c2fa8af5224ed
haloguyy123 <67880257+haloguyy123@users.noreply.github.com>
2020-08-05 08:04:37 -0400

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Windy=input('Is it windy outside? Pls answer True or False')
+Cold=input('Is it cold outside? Pls answer True or False')
+if Windy=='True' or Cold=='True': 
+    print('I think I should wear my jacket')
+else:
+    print('What a nice day!')
+
